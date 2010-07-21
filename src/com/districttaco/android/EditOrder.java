b5cfeb528402fc562b503b2e28fa933cca463bee
Jorge Velázquez <jorge.velazquez@gmail.com>
2010-07-20 19:28:15 -0700

    basic screens are in place and navigation between them has been added, along with menus where appropriate

--- src/com/districttaco/android/EditOrder.java
+++ src/com/districttaco/android/EditOrder.java
@@ -9,15 +9,15 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 
-public class ViewOrders extends Activity implements OnClickListener {
+public class EditOrder extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.view_orders);
+        setContentView(R.layout.edit_order);
 
         // set up click handlers
-        View createOrderButton = findViewById(R.id.create_order_button);
+        View createOrderButton = findViewById(R.id.add_item_button);
         createOrderButton.setOnClickListener((OnClickListener) this);
     }
         
@@ -25,24 +25,24 @@ public class ViewOrders extends Activity implements OnClickListener {
     public boolean onCreateOptionsMenu(Menu menu) {
     	super.onCreateOptionsMenu(menu);
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.view_orders_menu, menu);
+        inflater.inflate(R.menu.edit_order_menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-        case R.id.create_order:
-        	startActivity(new Intent(this, EditOrder.class));
+        case R.id.add_item:
+        	startActivity(new Intent(this, EditItem.class));
         	return true;
         }
         return super.onOptionsItemSelected(item);
     }
-    
+
     public void onClick(View v) {
     	switch (v.getId()) {
-    	case R.id.create_order_button:
-        	startActivity(new Intent(this, EditOrder.class));
+    	case R.id.add_item_button:
+        	startActivity(new Intent(this, EditItem.class));
     		break;
     	}
     }
