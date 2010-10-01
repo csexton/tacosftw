fcb885fdaf6ae8d3d7df9def98afccb93d1a8e02
Jorge Velázquez <jorge.velazquez@gmail.com>
2010-10-01 00:27:06 -0700

    working on the main screen, status fetches will eventually update the text

--- src/com/districttaco/android/Status.java
+++ src/com/districttaco/android/Status.java
@@ -1,18 +1,20 @@
 package com.districttaco.android;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.Date;
 
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.apache.http.HttpResponse;
-import org.apache.http.HttpEntity;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.json.JSONException;
 import org.json.*;
 
 public class Status {
+	static float lat = 0.0f;
+	static float lng = 0.0f;
+	static Date lastUpdate = null; 
+	
 	static public boolean UpdateStatus()
 	{
 		HttpClient client = new DefaultHttpClient();
@@ -24,8 +26,8 @@ public class Status {
 				JSONObject json = new JSONObject(body);
 				JSONObject status = json.getJSONArray("statuses").getJSONObject(0);
 				JSONObject location = status.getJSONObject("location");
-				String lat = location.get("latitude").toString();
-				String lng = location.get("longitude").toString();
+				lat = Float.parseFloat(location.get("latitude").toString());
+				lng = Float.parseFloat(location.get("longitude").toString());
 			} catch (JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
