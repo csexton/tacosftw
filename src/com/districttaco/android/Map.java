4d1ed0e9c6c2f7a39bf730c6e94f9ca55d9c6127
Jorge Velázquez <jorge.velazquez@gmail.com>
2010-10-02 15:35:22 -0700

    starting to build in map support

---
+++ src/com/districttaco/android/Map.java
@@ -0,0 +1,13 @@
+package com.districttaco.android;
+
+import com.google.android.maps.MapActivity;
+
+public class Map extends MapActivity {
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
