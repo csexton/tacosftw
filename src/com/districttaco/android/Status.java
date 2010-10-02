51cd4b78bf2025513dc779660011b50f759888a0
Jorge Velázquez <jorge.velazquez@gmail.com>
2010-10-01 23:22:02 -0700

    parsing out date so we know when the last update was

--- src/com/districttaco/android/Status.java
+++ src/com/districttaco/android/Status.java
@@ -1,6 +1,8 @@
 package com.districttaco.android;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -13,6 +15,8 @@ import org.json.*;
 public class Status {
 	static private double lat = 0.0f;
 	static private double lng = 0.0f;
+	static private String locationName = null;
+	static private String locationDescription = null;
 	static private String statusText = null;
 	static private Date lastUpdate = null;
 	static private String infoTitle = null;
@@ -30,11 +34,20 @@ public class Status {
 				// extract the contents from the json feed
 				JSONObject json = new JSONObject(body);
 				JSONObject status = json.getJSONArray("statuses").getJSONObject(0);
+				SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssz");
+				try {
+					lastUpdate = formatter.parse(status.getString("updated_at"));
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 				JSONObject info = status.getJSONObject("info");
 				infoTitle = info.getString("title");
 				infoHeader = info.getString("header");
 				infoBody = info.getString("body");
 				JSONObject location = status.getJSONObject("location");
+				locationName = location.getString("name");
+				locationDescription = location.getString("description");
 				lat = location.getDouble("latitude");
 				lng = location.getDouble("longitude");
 				statusText = status.getString("body");
@@ -50,6 +63,14 @@ public class Status {
 		}
 		
 		return true;
+	}
+	
+	static String getLocationName() {
+		return locationName;
+	}
+	
+	static String getLocationDescription() {
+		return locationDescription;
 	}
 	
 	static double getLatitude() {
