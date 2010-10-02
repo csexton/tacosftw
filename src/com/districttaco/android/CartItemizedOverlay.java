ecffaee84cf17cf7623e8bf6390bfeef3ec9cf79
Jorge Velázquez <jorge.velazquez@gmail.com>
2010-10-02 16:57:46 -0700

    got map working, need to set zoom level and center

---
+++ src/com/districttaco/android/CartItemizedOverlay.java
@@ -0,0 +1,35 @@
+package com.districttaco.android;
+
+import java.util.ArrayList;
+
+import android.graphics.drawable.Drawable;
+
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.OverlayItem;
+
+public class CartItemizedOverlay extends ItemizedOverlay {
+	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
+
+	public CartItemizedOverlay(Drawable defaultMarker) {
+		super(boundCenterBottom(defaultMarker));
+		// TODO Auto-generated constructor stub
+	}
+
+	public void addOverlay(OverlayItem overlay) {
+	    mOverlays.add(overlay);
+	    populate();
+	}
+	
+	@Override
+	protected OverlayItem createItem(int i) {
+		// TODO Auto-generated method stub
+		return mOverlays.get(i);
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return mOverlays.size();
+	}
+
+}
--- src/com/districttaco/android/Map.java
+++ src/com/districttaco/android/Map.java
@@ -1,8 +1,34 @@
 package com.districttaco.android;
 
-import com.google.android.maps.MapActivity;
+import java.util.List;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+
+import com.google.android.maps.*;
 
 public class Map extends MapActivity {
+	private MapView mapView = null;
+	private List<Overlay> mapOverlays;
+	private CartItemizedOverlay cartOverlay;
+	private Drawable drawable;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map);
+        
+        mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
+        
+        GeoPoint point = new GeoPoint((int)(Status.getLatitude() * 1E6), (int)(Status.getLongitude() * 1E6));
+        OverlayItem overlayItem = new OverlayItem(point, "", "");
+        mapOverlays = mapView.getOverlays();
+        drawable = this.getResources().getDrawable(R.drawable.cartmarker);
+        cartOverlay = new CartItemizedOverlay(drawable);
+        cartOverlay.addOverlay(overlayItem);
+        mapOverlays.add(cartOverlay);
+    }
 
 	@Override
 	protected boolean isRouteDisplayed() {
