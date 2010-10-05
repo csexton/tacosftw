0be2eb77a86a896f2ba000079843c603d14947fc
Jorge Velázquez <jorge.velazquez@gmail.com>
2010-10-05 09:55:34 -0700

    removing the order stuff for now, will revisit it if/when we decide to add that functionality, but no sense in bloating the package until that time

--- src/com/districttaco/android/Order.java
+++
@@ -1,8 +0,0 @@
-package com.districttaco.android;
-
-import java.util.Date;
-
-public class Order {
-	private OrderItem[] items = null;
-	private Date lastSubmitted = null;
-}
--- src/com/districttaco/android/OrderItem.java
+++
@@ -1,6 +0,0 @@
-package com.districttaco.android;
-
-public class OrderItem {
-	private int quantity = 0;
-	private String name = null;
-}
