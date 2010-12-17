543fa65002b6ce046f6c0246d402ff9fb4be9718
Jorge Velázquez <jorge.velazquez@gmail.com>
2010-12-16 19:50:05 -0800

    added new icon for the map, and map is now showing the restaurant location at least, and cart information if it is available

--- chrome/js/dt.js
+++ chrome/js/dt.js
@@ -3,20 +3,38 @@
 $(document).ready(function() {
 	// create the map
 	var options = {
-		zoom: 6,
+		zoom: 11,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	var map = new google.maps.Map(document.getElementById('map'), options);
 	
 	// get the cart status
 	$.getJSON('http://carte.districttaco.com/status.json', function(data) {
+		// center on the restaurant
+		var address = '5723 N. Lee Highway, Arlington, VA 22207';
+		var geocoder = new google.maps.Geocoder();
+		geocoder.geocode({'address': address}, function (results, status) {
+			if (status == google.maps.GeocoderStatus.OK) {
+				map.setCenter(results[0].geometry.location);
+				var infowindow = new google.maps.InfoWindow({
+					content: '<b>District Taco Restaurant</b><br />5723 N. Lee Highway<br />Arlington, VA 22207'
+				});
+				var marker = new google.maps.Marker({
+					map: map,
+					position: results[0].geometry.location,
+					icon: '/images/icon_48.png'
+				});
+				google.maps.event.addListener(marker, 'click', function() {
+					infowindow.open(map, marker);
+				});
+			}
+		});
 		$.each(data['statuses'], function() {
 			var body = this['body'];
 			var location = this['location'];
 			if (location['latitude'] != 0 && location['longitude'] != 0) {
-//				map.setCenter(new google.maps.LatLng(location['latitude'], location['longitude']));
-//				alert('hi');
 			}
 		});
+		return false;
 	});
 });
